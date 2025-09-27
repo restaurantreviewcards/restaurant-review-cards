c75abd1cf821fc074f530d5aa001ab19f9b46f11
restaurantreviewcards <jake@restaurantreviewcards.com>
2025-09-27 16:29:37 -0400

    neww

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -15,28 +15,21 @@ exports.handler = async (event) => {
       throw new Error('Stripe Price ID, email, or Place ID is missing.');
     }
 
-    const customer = await stripe.customers.create({
-      email: email,
-    });
+    const customer = await stripe.customers.create({ email: email });
 
-    // **THE FIX IS HERE**: We now expand the 'pending_setup_intent'
     const subscription = await stripe.subscriptions.create({
       customer: customer.id,
       items: [{ price: priceId }],
       payment_behavior: 'default_incomplete',
       payment_settings: { save_default_payment_method: 'on_subscription' },
-      expand: ['pending_setup_intent'], // This is the key change
-      metadata: {
-        placeId: placeId,
-        email: email
-      }
+      expand: ['pending_setup_intent'],
+      metadata: { placeId: placeId, email: email }
     });
 
     return {
       statusCode: 200,
       body: JSON.stringify({
         subscriptionId: subscription.id,
-        // And we get the client secret from the correct object
         clientSecret: subscription.pending_setup_intent.client_secret,
       }),
     };
