06e82835884c8820f44b0778f3f68f456c02a653
restaurantreviewcards <jake@restaurantreviewcards.com>
2025-09-27 16:33:50 -0400

    neww

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -22,15 +22,27 @@ exports.handler = async (event) => {
       items: [{ price: priceId }],
       payment_behavior: 'default_incomplete',
       payment_settings: { save_default_payment_method: 'on_subscription' },
-      expand: ['pending_setup_intent'],
-      metadata: { placeId: placeId, email: email }
+      expand: ['latest_invoice.payment_intent'], // We expand this for the next step
+      metadata: {
+        placeId: placeId,
+        email: email
+      }
     });
+
+    // **THIS IS THE NEW, MORE ROBUST METHOD**
+    // We get the client secret from the invoice that was just created for the subscription.
+    // This is the most reliable way to get the client secret.
+    const clientSecret = subscription.latest_invoice.payment_intent.client_secret;
+
+    if (!clientSecret) {
+        throw new Error('Could not retrieve client_secret from the subscription invoice.');
+    }
 
     return {
       statusCode: 200,
       body: JSON.stringify({
         subscriptionId: subscription.id,
-        clientSecret: subscription.pending_setup_intent.client_secret,
+        clientSecret: clientSecret,
       }),
     };
     
